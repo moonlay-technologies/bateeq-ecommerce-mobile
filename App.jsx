0133db396ab5df52dcb3e48ebdb04e821fe700c2
krisna dwi setyaadi <krisnadwisetyadi@gmail.com>
2023-06-14 10:07:24 +0700

    fix undefined

--- App.jsx
+++ App.jsx
@@ -1,43 +1,45 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Animated } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { connect } from 'react-redux';
 import Routes from './app/navigations/Route';
-import {Animated} from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import {connect} from "react-redux";
-import {LoadUsers, setToken} from "./app/store/actions/user";
+import { LoadUsers, setToken } from './app/store/actions/user';
 
 function App({ ...props }) {
-  let { options,LoadUsers,setToken,loading, isAuthenticated } = props
-  const [newLoad,setNewLoad] = useState(true)
+  const { options, LoadUsers, setToken, loading, isAuthenticated } = props;
+  const [newLoad, setNewLoad] = useState(true);
   const [newToken, setNewToken] = useState(null);
 
-  useEffect(()=> {
-    setNewLoad(true)
+  useEffect(() => {
+    setNewLoad(true);
     AsyncStorage.getItem('accessToken')
-        .then(val => {
-          if(val){
-            setToken(val)
-            setNewToken(val)
-          }
-          setNewLoad(false)
-        })
-        .catch((err)=> {
-          setNewLoad(false)
-        })
-  },[setToken])
-
-
-  useEffect(()=> {
-    if(!newLoad){
-      LoadUsers({
-        accessToken: newToken
+      .then(val => {
+        if (val) {
+          setToken(val);
+          setNewToken(val);
+        }
+        setNewLoad(false);
       })
+      .catch(err => {
+        setNewLoad(false);
+      });
+  }, [setToken]);
+
+  useEffect(() => {
+    if (!newLoad) {
+      LoadUsers({
+        accessToken: newToken,
+      });
     }
-  },[LoadUsers,newLoad,newToken])
+  }, [LoadUsers, newLoad, newToken]);
 
   return <Routes />;
 }
-export default connect(({Auth,User})=> {
-  let { isAuthenticated } = Auth
-  let { options,loading } = User
-  return {options ,isAuthenticated,loading }
-},{LoadUsers,setToken})(App)
+export default connect(
+  ({ Auth, User }) => {
+    const { isAuthenticated } = Auth;
+    const { options, loading } = User;
+    return { options, isAuthenticated, loading };
+  },
+  { LoadUsers, setToken }
+)(App);
